87f88d047605dd21f9cde7d9a9f30149201ffc01
shmiki <shmiki@microsoft.com>
2024-08-28 22:44:08 +0900

    remove maxLogSize

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -3,7 +3,6 @@ const path = require("path");
 const APP_ROOT = path.join(__dirname, "../");
 const logFileName = process.env.LOG_FILE_NAME || 'application.log';
 const logFilePath = process.env.LOG_FILE_PATH || 'logs';
-const maxLogSize = parseInt(process.env.MAX_LOG_SIZE, 10) || 10485760; // Default to 10MB if not set
 const backups = parseInt(process.env.LOG_ROTATE, 10) || 3; // Default to 3 backups if not set
 const logLevel = process.env.LOG_LEVEL || 'debug'; // Default to 'debug' if not set
 
@@ -14,7 +13,6 @@ log4js.configure({
             type: 'dateFile',
             filename: path.join(APP_ROOT, logFilePath, logFileName),
             pattern: '.yyyy-MM-dd.log',
-            //maxLogSize: maxLogSize,
             numBackups: backups, // Replaced daysToKeep with numBackups
             alwaysIncludePattern : true,
 
