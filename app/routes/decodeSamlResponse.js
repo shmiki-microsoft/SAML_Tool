5109ee9ab4218122bf7f467958a6d5b4c49e51cf
shmiki <shmiki@microsoft.com>
2025-04-07 18:52:32 +0900

    SAML: Update decoding routes and add new response/request view

--- app/routes/decodeSamlResponse.js
+++ app/routes/decodeSamlResponse.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { decodeSamlResponse } = require('../services/samlService');
+const { decodeSamlResponse,decodeSamlRequest } = require('../services/samlService');
 const logger = require('../utils/logger');
 const handleError = require('../utils/errorHandler');
 
-router.get('/decodeSamlResponse', (req, res) => {
-    logger.info('Received GET request on /decodeSamlResponse');
-    res.render('decodeSamlResponse', { samlResponse:null, decodedResponse:null });
+router.get('/decodeSamlRequestandResponse', (req, res) => {
+    logger.info('Received GET request on /decodeSamlRequestandResponse');
+    res.render('decodeSamlRequestandResponse', { samlResponse:null, decodedResponse:null, samlRequest:null, decodedRequest:null});
 });
 
 router.post('/decodeSamlResponse', (req, res) => {
@@ -17,7 +17,21 @@ router.post('/decodeSamlResponse', (req, res) => {
         const decodedResponse = decodeSamlResponse(samlResponse);
         logger.info('SAML response decoded successfully');
         logger.debug('Decoded SAML response:', decodedResponse);
-        res.render('decodeSamlResponse', { samlResponse, decodedResponse});
+        res.render('decodeSamlRequestandResponse', { samlResponse, decodedResponse, samlRequest:null,decodedRequest:null});
+    } catch (err) {
+        handleError(res, err, 500, 'Failed to decode SAML response');
+    }
+});
+
+router.post('/decodeSamlRequest', async (req, res) => {
+    const { samlRequest } = req.body;
+    logger.info('Received POST request on /decodeSamlResponse');
+    logger.debug('Request body:', req.body);
+    try {
+        const decodedRequest = await decodeSamlRequest(samlRequest);
+        logger.info('SAML response decoded successfully');
+        logger.debug('Decoded SAML response:', decodedRequest);
+        res.render('decodeSamlRequestandResponse', { samlResponse:null, decodedResponse:null, samlRequest, decodedRequest});
     } catch (err) {
         handleError(res, err, 500, 'Failed to decode SAML response');
     }
