bcc4a4532426d870cafea3ea039eeb56662d3914
shmiki <shmiki@microsoft.com>
2025-04-09 10:07:52 +0900

    Add usage instructions

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,10 @@ const logger = require('../utils/logger');
 
 router.get('/', (req, res) => {
     logger.info('Received GET request on /');
-    const envVars = initializeEnvironmentVariables();
-    logger.debug('Environment variables:', envVars);
-    res.render('generateSamlRequest', envVars);
+    // const envVars = initializeEnvironmentVariables();
+    // logger.debug('Environment variables:', envVars);
+    // res.render('generateSamlRequest', envVars);
+    res.render('index',{WIKI_LINK:process.env.WIKI_LINK})
 });
 
 module.exports = router;
